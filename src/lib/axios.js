674446d27cc865294745e1e03b57abd35c25c7cc
Peter <peterchinokoo@gmail.com>
2025-01-26 21:50:12 +0200

    change the url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://ecommerce-web-app-y0xh.onrender.com/api",
+  baseURL: [
+    "https://ecommerce-web-app-y0xh.onrender.com/api",
+    "http://localhost:3001/api",
+  ],
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
@@ -9,4 +12,4 @@ export const axiosInstance = axios.create({
 });
 
 //development
-//http://localhost:3001/api
+//
